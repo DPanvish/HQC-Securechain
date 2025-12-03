0e45f602c6ace07291f689e6ddfbb0fe045123dc
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-12-03 21:02:24 +0530

    Config

---
+++ dashboard/src/config.js
@@ -0,0 +1,24 @@
+export const RPC_URL = "http://127.0.0.1:8545";
+
+export const QSW_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+export const QSW_ABI = [
+  {
+    "inputs": [
+      { "internalType": "address", "name": "_owner", "type": "address" }
+    ],
+    "name": "getAccount",
+    "outputs": [
+      { "internalType": "address", "name": "owner", "type": "address" },
+      { "internalType": "bytes", "name": "pqPublicKey", "type": "bytes" },
+      {
+        "internalType": "enum QuantumSafeWallet.Mode",
+        "name": "mode",
+        "type": "uint8"
+      },
+      { "internalType": "bool", "name": "exists", "type": "bool" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
